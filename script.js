00a69cfbd5942f4dbdbf895b4d0efd8f2365f510
Aris83232 <64313792+Aris83232@users.noreply.github.com>
2024-08-16 12:46:08 +0530

    Synced the spawn with mouse movement.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ bodyEL.addEventListener("mousemove", (event)=>{
     const xPos = event.offsetX;
     const yPos = event.offsetY;
     const spanEl = document.createElement("span");
-    spanEl.style.left = yPos + "px";
-    spanEl.style.top = xPos + "px";
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
     bodyEL.appendChild(spanEl);
 })
