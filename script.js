c4f6d4a6cb8402fbd2b45c29596a3ac4eec809f1
Aris83232 <64313792+Aris83232@users.noreply.github.com>
2024-08-16 17:27:34 +0530

    Randomized ICON size

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ bodyEL.addEventListener("mousemove", (event)=>{
     const xPos = event.offsetX;
     const yPos = event.offsetY;
     const spanEl = document.createElement("span");
+
+    //For Position
     spanEl.style.left = xPos + "px";
     spanEl.style.top = yPos + "px";
+
+    //For random Size of the icon
+    const size = Math.random()*100 + 25;
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px";
+
+    //For deletion of the element after a set period of time
     bodyEL.appendChild(spanEl);
+    setTimeout(()=>{
+        spanEl.remove()
+    }, 3000)
 })
